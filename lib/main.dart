22e86da5474c74194638155bc41689033f10e73c
Aditya Deopurkar <deopurkaraditya@gmail.com>
2021-01-20 00:02:17 +0530

    Added drawer and bottom navigation bar:

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/screen/meal_detail_screen.dart';
 import 'screen/categories_screen.dart';
 import 'screen/category_meals_screen.dart';
+import './screen/tabs_screen.dart';
+import './screen/filters_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,27 +13,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.amber,
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        fontFamily: 'Raleway',
-        textTheme: ThemeData.light().textTheme.copyWith(
-          bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-          bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-          subtitle1: TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondensed',
-            fontWeight: FontWeight.bold
-          )
-        )
-      ),
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              subtitle1: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold))),
       // home: CategoriesScreen(),
       initialRoute: '/',
       routes: {
-        '/': (ctx) =>  CategoriesScreen(),
-        CategoryMealsScreen.routeName: (ctx) =>  CategoryMealsScreen(),
-        MealDetailScreen.routeName: (ctx) =>  MealDetailScreen()
+        '/': (ctx) => TabsScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen()
       },
+      // onGenerateRoute: (settings) {
+      //   print(settings.arguments);
+      //   return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      // },
+      onUnknownRoute: (settings){
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      } ,
     );
   }
 }
---
+++ lib/screen/filters_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
+
+class FiltersScreen extends StatelessWidget {
+  static const routeName = '/filters';
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Yours Filters'),),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('Filter Screen!'),
+      ),
+    );
+  }
+}
